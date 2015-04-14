6208e71e60e4ca3d7e6fa2e863427a39205d0723
ram757 <rmcdonald144@gmail.com>
2015-04-14 02:22:13 -0400

    Creating House Class
    House Class Test class and House Class created to start being worked on

---
+++ src/House.java
@@ -0,0 +1,12 @@
+
+public class House
+{
+    private Room[][] _floorPlan;
+    private int _rowLoc = 0;
+    private int _colLoc = 0;
+    
+    private int _endRow = 0;
+    private int _endCol = 0;
+    
+
+}
