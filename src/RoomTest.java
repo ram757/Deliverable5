cb50aced43794c90d881375e784ac6db4b57aa6f
ram757 <rmcdonald144@gmail.com>
2015-04-14 00:04:31 -0400

    Tests in RoomTest
    Added Tests in RoomTest

--- src/RoomTest.java
+++ src/RoomTest.java
@@ -11,13 +11,40 @@ public class RoomTest
     @Mock
     Trap mockTrap = Mockito.mock(Trap.class);
     Item mockItem = Mockito.mock(Item.class);
-    Room room;
+    String description;
+    String peekDescription;
+    String itemUsable;
     
     @Before
     public void setUp() throws Exception
     {
         MockitoAnnotations.initMocks(mockTrap);
         MockitoAnnotations.initMocks(mockItem);
+        
+        description = "You are in a room full of clowns with cat ears.";
+        peekDescription = "Listening at the door, you hear cat sounds.";
+        itemUsable = "peanut butter";
     }
+    
+    @Test
+    public void testConstructorWithNoItemTrap()
+    {
+        Room room = new Room(description, peekDescription);
+        
+        assertTrue(room != null);
+    }
+    
+    @Test
+    public void testConstructorWithItemTrap()
+    {
+        Room room = new Room(description, peekDescription, mockItem, mockTrap, itemUsable);
+        
+        assertTrue(room != null);
+    }
+    
+    
+    
+    
+    
 
 }
