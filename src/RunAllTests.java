0f1fac1dee817400608ac8fcba69cddccb61ae64
ram757 <rmcdonald144@gmail.com>
2015-04-14 02:19:25 -0400

    Run all tests so far
    Created a "RunAllTests" program to run all of the test cases so far to
    verify everything continues to work as it is being built.

---
+++ src/RunAllTests.java
@@ -0,0 +1,12 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TrapTest.class, ItemTest.class, PlayerTest.class, RoomTest.class })
+
+public class RunAllTests
+{
+
+}
