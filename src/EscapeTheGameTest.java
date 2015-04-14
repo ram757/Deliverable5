4c5f3aa703cf2997ad90c3b06f2040db5a0e2fe8
ram757 <rmcdonald144@gmail.com>
2015-04-14 10:10:41 -0400

    EscapeTheGame: Class
    Created interfacing class and wrote some methods for parsing data.  Will
    not be able to finish this..

--- src/EscapeTheGameTest.java
+++ src/EscapeTheGameTest.java
@@ -13,4 +13,87 @@ public class EscapeTheGameTest
         assertEquals(0, game.runIntroduction("Name"));
     }
 
+    @Test
+    /*
+     * Test that dimensions can be parsed from String when
+     * dimensions are parsable
+     */
+    public void testParseDimensionsFromString()
+    {
+        int row = 5;
+        int col = 9;
+        
+        EscapeTheGame escapeGame = new EscapeTheGame();
+        
+        String dimensionString = (row + ";" + col);
+        int[] dimensions = escapeGame.parseDimensionsFromString(dimensionString);
+        
+        assertTrue(dimensions[0] == row);
+        assertTrue(dimensions[1] == col);
+    }
+    
+    @Test
+    /*
+     * Test that parsing Item from String returns an Item when 
+     * Item is parsable
+     */
+    public void testParseItemFromStringWithItem()
+    {
+        String itemAsString = "key;This is a black key; Using key unlocks door";
+        
+        EscapeTheGame escapeGame = new EscapeTheGame();
+        
+        Item item = escapeGame.parseItemFromString(itemAsString);
+        
+        assertNotNull(item);
+    }
+    
+    @Test
+    /*
+     * Test that parsing Item from String returns null when 
+     * item is not parsable
+     */
+    public void testParseItemFromStringWithoutItem()
+    {
+        String itemAsString = "none";
+        
+        EscapeTheGame escapeGame = new EscapeTheGame();
+        
+        Item item = escapeGame.parseItemFromString(itemAsString);
+        
+        assertNull(item);
+    }
+    
+    @Test
+    /*
+     * Test that parsing Trap from String returns a Trap when 
+     * trap is parsable
+     */
+    public void testParseTrapFromStringWithTrap()
+    {
+        String trapAsString = "dead;You encounter a hoard of sorority chicks.";
+        
+        EscapeTheGame escapeGame = new EscapeTheGame();
+        
+        Trap trap = escapeGame.parseTrapFromString(trapAsString);
+        
+        assertNotNull(trap);
+    }
+    
+    @Test
+    /*
+     * Test that parsing Trap from String returns Null when 
+     * there is no Trap parsable
+     */
+    public void testParseTrapFromStringWithoutTrap()
+    {
+        String trapAsString = "none";
+        
+        EscapeTheGame escapeGame = new EscapeTheGame();
+        
+        Trap trap = escapeGame.parseTrapFromString(trapAsString);
+        
+        assertNull(trap);
+    }
+    
 }
