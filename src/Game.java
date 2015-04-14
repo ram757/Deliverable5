be7be7d1d9533cf46bbbe81c28ef5b34387143af
ram757 <rmcdonald144@gmail.com>
2015-04-14 10:25:51 -0400

    Creating Game Class
    Game Test and Game Class created

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Game
+{
+    private Player _player = null;
+    private House _house = null;
+    private Pattern pattern = Pattern.compile("\\s*([a-z]+)\\s*([a-z]*)");
+    private Scanner console = new Scanner(System.in);
+
+    
+}
