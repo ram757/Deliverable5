56e5d4809789f0c9e8a1bbca1bcfcb60f7388bc5
ram757 <rmcdonald144@gmail.com>
2015-04-14 06:57:24 -0400

    Create setup for houseTests
    Setup method to initialize a mocked matrix of rooms for house

--- src/HouseTest.java
+++ src/HouseTest.java
@@ -1,13 +1,35 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
-
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class HouseTest
 {
 
+    Room[][] gameboard;
+    int numOfRows = 5;
+    int numOfCols = 5;
+    
+    @Before
+    public void setUp()
+    {
+        for(int i = 0; i < numOfRows; i++)
+        {
+            for(int j = 0 ; j < numOfCols; j++)
+            {
+                gameboard[i][j] = Mockito.mock(Room.class);
+                MockitoAnnotations.initMocks(gameboard[i][j]);
+            }
+        }
+    }
+    
+    
     @Test
-    public void test()
+    public void testHouseConstructorGivenBoard()
     {
         fail("Not yet implemented");
     }
