c752836f110d17e21e553c5602487d765981b59d
ram757 <rmcdonald144@gmail.com>
2015-04-14 00:27:54 -0400

    Constructing Room toString()
    Coding up toString's and whatnot

--- src/RoomTest.java
+++ src/RoomTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
@@ -40,6 +41,64 @@ public class RoomTest
         Room room = new Room(description, peekDescription, mockItem, mockTrap, itemUsable);
         
         assertTrue(room != null);
+    }
+    
+    @Test
+    public void testRoomHasItem()
+    {
+        Room room = new Room(description, peekDescription, mockItem, mockTrap, itemUsable);
+        boolean expectedValue = true;
+        boolean returnedValue = room.hasItem();
+        
+        assertEquals(expectedValue, returnedValue);
+    }
+    
+    @Test
+    public void testRoomHasTrap()
+    {
+        Room room = new Room(description, peekDescription, mockItem, mockTrap, itemUsable);
+        boolean expectedValue = true;
+        boolean returnedValue = room.hasTrap();
+        
+        assertEquals(expectedValue, returnedValue);
+    }
+    
+    @Test
+    public void testRoomToStringWithoutItemAndTrap()
+    {
+        String trapString = "Eaten by cats";
+        String itemString = "unicycle";
+        
+        Room room = new Room(description, peekDescription);
+        
+        String expectedString = ("Room:\n\tDescription: " + description);
+        
+        String returnedString = room.toString();
+        
+        assertEquals(expectedString, returnedString);
+    }
+    
+    
+    @Test
+    public void testRoomToStringWithItemAndTrap()
+    {
+        String trapString = "Eaten by cats";
+        String itemString = "unicycle";
+        
+        when(mockItem.toString()).thenReturn(itemString);
+        when(mockTrap.toString()).thenReturn(trapString);
+        
+        Room room = new Room(description, peekDescription, mockItem, mockTrap, itemUsable);
+        
+        String expectedString = ("Room:\n\tDescription: " +
+                                description +
+                                "\n\tTrap: " + trapString +
+                                "\n\tItem: " + itemString +
+                                "\n\tItem Usable: " + itemUsable);
+        
+        String returnedString = room.toString();
+        
+        assertEquals(expectedString, returnedString);
     }
     
     
